65e0ded2b7821fcd5ae53d4c6e664153e9481c1c
nikraev <nikraev@yandex.ru>
2015-06-10 11:26:44 +0300

    Add WorkQueue class

--- workqueues.rb
+++ workqueues.rb
@@ -9,10 +9,18 @@ class WorkQueuesRabbit < RabbitMQTransmission
     queue = getQueue(name_queue)
     queue.subscribe(:manual_ack => true, :block => true) do |delivery_info, properties, body|
       puts " [x] Received #{body}"
-      sleep body.count(".").to_i
-      puts " [x] Done"
-      @chanelRabbit.ack(delivery_info.delivery_tag)
+      
+      30.times do |i|
+        sleep 1
+        puts "#{i+1} seconds"
+        puts "Delivery time"  
+      end
+      
+      sendAck(delivery_info)
+      puts "Delivery complite"
     end  
+    closeRabbit()      
+    
   end
   
   def SendMessage(message,name_queue)
@@ -22,10 +30,14 @@ class WorkQueuesRabbit < RabbitMQTransmission
       true
   end
   
+private 
+    def sendAck(delivery_info)
+      @chanelRabbit.ack(delivery_info.delivery_tag)
+    end
 end 
 
 
 wqueue = WorkQueuesRabbit.new
 
-wqueue.SendMessage("hello","workdata")
+wqueue.SendMessage("mail agent","workdata")
 wqueue.ReciveMessage("workdata")
